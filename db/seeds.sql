64acc61f7bbfe177a4e104183a5e087cf496c02a
Bren-lap <brennenbates2859@gmail.com>
2019-11-11 18:26:59 -0700

    Initial file creations
    initial file creations

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name,devoured) 
+VALUES('Veggie Burger', false);
+
+INSERT INTO burgers(burger_name,devoured) 
+VALUES('Big Benny', false);
+
+INSERT INTO burgers(burger_name,devoured) 
+VALUES('PB and J Burger', false);
+
+
