56c5b9e827410eaeacb3a99744e2f7875a7599b0
Bren <brennenbates2859@gmail.com>
2019-11-18 11:45:01 -0700

    Main programming session
    Set up Eat-Da-Burger app using Model View Controller (MVC) methodology with an Object Relational Mapper (ORM).

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
-    devoured BOOL NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY(id)
 );
 
