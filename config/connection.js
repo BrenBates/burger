2205f00c371c5e621f66752d0cb5c8d8b95488b7
Bren <brennenbates2859@gmail.com>
2019-11-19 13:13:33 -0700

    Update connection.js
    trying with just pool

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,19 @@
 // Export the connection.
 var mysql = require("mysql");
 
+const pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
    
-if(process.env.CLEARDB_DATABASE_URL) {
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-    const pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
-} else {
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
-}
+// if(process.env.CLEARDB_DATABASE_URL) {
+//     connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+// } else {
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "burgers_db"
+// });
+// }
 
 // Make connection.
 connection.connect(function (err) {
