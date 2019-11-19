6f11968f18f8a29f97544ae2a5562a147a2d60ca
Bren <brennenbates2859@gmail.com>
2019-11-19 13:40:10 -0700

    Update connection.js
    troubleshooting issue where heroku app will initially work upon deployment but then the server will crash after a minute or two. Adding in connection to a function and then putting a statement to reconnect upon error.

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,29 @@
 // Export the connection.
 var mysql = require("mysql");
 
-connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-   
-// if(process.env.CLEARDB_DATABASE_URL) {
-//     connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-// } else {
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-// });
-// }
+
+var connection;
+
+
+function connectDb() {
+
+if(process.env.CLEARDB_DATABASE_URL) {
+    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+}
+
+    connection.on('error', connectDb());
+}
+
+connectDb();
 
 // Make connection.
 connection.connect(function (err) {
@@ -25,7 +35,6 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
-connection.query('select 1 + 1', (err, rows) => { /* */ });
 
 // Export connection for our ORM to use.
 module.exports = connection;
